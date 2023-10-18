5010a4fd9810cb32c0848bfa3c1c24991437feae
N3g1n <neginfirouzi2000@gmail.com>
2023-10-18 10:58:26 +0330

    Remove different is_player_win code

--- main.py
+++ main.py
@@ -22,46 +22,6 @@ class TicTacToe:
         else:
             self.board[row][col] = player
 
-    # def is_player_win(self, player):
-    #     win = None
-    #
-    #     n = len(self.board)
-    #
-    #     for i in range(n):
-    #         win = True
-    #         for j in range(n):
-    #             if self.board[i][j] != player:
-    #                 win = False
-    #                 break
-    #         if win:
-    #             return win
-    #
-    #     for i in range(n):
-    #         win = True
-    #         for j in range(n):
-    #             if self.board[j][i] != player:
-    #                 win = False
-    #                 break
-    #         if win:
-    #             return win
-    #
-    #     # checking diagonals
-    #     win = True
-    #     for i in range(n):
-    #         if self.board[i][i] != player:
-    #             win = False
-    #             break
-    #     if win:
-    #         return win
-    #
-    #     win = True
-    #     for i in range(n):
-    #         if self.board[i][n - 1 - i] != player:
-    #             win = False
-    #             break
-    #     if win:
-    #         return win
-    #     return False
 
     def is_player_win(self, player):
         # Check rows
